0874b839b4a0611c46523500d58f7da56bd551a0
sabhinho <marinadu789@gmail.com>
2023-11-29 16:40:32 +0100

    var to let

--- web/index.js
+++ web/index.js
@@ -1,30 +1,30 @@
 // FILE INPUT
 document.addEventListener('DOMContentLoaded', function () {
     // Sélectionnez l'élément d'entrée de fichier
-    var fileInput = document.getElementById('fileInput');
+    let fileInput = document.getElementById('fileInput');
 
     // Ajoutez un écouteur d'événements 'change' à l'élément d'entrée de fichier
     fileInput.addEventListener('change', handleFileSelect);
 
     function handleFileSelect(event) {
         // Obtenez l'élément d'entrée de fichier
-        var input = event.target;
+        let input = event.target;
 
         // Assurez-vous qu'un fichier a été sélectionné
         if (input.files && input.files[0]) {
             // Accédez au fichier sélectionné
-            var selectedFile = input.files[0];
+            let selectedFile = input.files[0];
 
             // AUDIO PLAYER 
-            var audio = document.getElementById("audioPlayer");
+            let audio = document.getElementById("audioPlayer");
 
             // Vérifiez si l'élément audio existe
             if (audio) {
                 // Vérifiez si la balise source existe à l'intérieur de l'élément audio
-                var source = audio.querySelector('source');
+                let source = audio.querySelector('source');
 
                 // Créez une URL pour le fichier audio sélectionné
-                var audioURL = URL.createObjectURL(selectedFile);
+                let audioURL = URL.createObjectURL(selectedFile);
 
                 // Mettez le fichier audio sélectionné dans la balise source
                 source.src = audioURL;
@@ -41,6 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // AUDIO PLAYER 
 
-var audio = document.getElementById("audioPlayer");
-var source = document.getElementById("audioSource");
+let audio = document.getElementById("audioPlayer");
+let source = document.getElementById("audioSource");
 
